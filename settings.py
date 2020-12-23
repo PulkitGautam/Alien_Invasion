44badd1c6bd81cd5c4cffcf49836cc3cf9d9fb0b
PulkitGautam <pulkitgautam02@gmail.com>
2020-12-23 21:16:52 +0530

    Alien_Invasion

---
+++ settings.py
@@ -0,0 +1,23 @@
+# A class to store all settings for Alien Invasion
+class Settings:
+
+    def __init__(self):     # Initialize the game's settings
+        # Screen settings
+        self.screen_width = 1366
+        self.screen_height = 768
+        self.bg_color = (230, 230, 230)
+
+        # Ship Setting
+        self.ship_speed = 0.5
+
+        # Bullet Setting
+        self.bullet_speed = 1.5
+        self.bullet_allowed = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
+        # Alien Setting
+        self.alien_speed = 0.5
+        self.alien_drop_speed = 10
+        self.fleet_direction = 1                         # 1 = Right, -1 = Left
---
+++ ship.py
@@ -0,0 +1,35 @@
+import pygame
+
+# A Class to Manage the Spaceship
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()       # In pygame all elements are treated as Rectangle
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Decimal Value for Ships Horizontal Position
+        self.x = float(self.rect.x)
+
+        # Flag for allowin Continous Movement
+        self.moving_right = False
+        self.moving_left = False
+        
+    # Update the ship's position based on the movement flag.
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > self.screen_rect.left:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
